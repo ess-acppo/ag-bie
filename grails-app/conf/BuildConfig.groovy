8cf8d92b48cd95b7540049a64d5affb515ee0583
Dave Martin <djtfmartin@gmail.com>
2015-07-09 14:39:18 +0100

    release plugin + CSS

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,17 +10,8 @@ grails.plugin.location."bie-plugin" = "../bie-plugin"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+        test: false,
+        run: false
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy
@@ -41,6 +32,10 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        build(":release:3.0.1",
+                ":rest-client-builder:2.0.3") {
+            export = false
+        }
         // plugins for the build system only
         build ":tomcat:7.0.55"
         compile ':cache:1.1.8'
